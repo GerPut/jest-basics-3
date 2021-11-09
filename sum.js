8ae226469087a39cf3a56f6f1f28b50479bbdebc
Gerhard <gerhard.putter@yahoo.com>
2021-11-09 10:08:32 +0200

    Truthy and Falsy.

--- sum.js
+++ sum.js
@@ -2,4 +2,10 @@ function sum(num1, num2) {
     return num1 + num2
 }
 
+if (false) {
+    console.log("this is truthy")
+} else {
+    console.log("this is falsey")
+}
+
 module.exports = sum
--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,25 @@
 const sum = require("./sum")
 
-it("should add 1 + 2 = 3", () => {
-    const result = sum(1, 2);
-    expect(result).toBe(3)
+describe("example tests", () => {
+    it("should add 1 + 2 = 3", () => {
+        const result = sum(1, 2);
+        expect(result).toBe(3)
+    })
+
+    it("object assignment", () => {
+        const obj = {};
+        expect(obj).toEqual({})
+    })
+
+
+})
+
+//Truthy or Falsy Values
+
+describe("truthy or falsy", () => {
+    it("null", () => {
+        const n = null;
+        expect(n).toBeFalsy()
+        expect(n).not.toBeTruthy()
+    })
 })
