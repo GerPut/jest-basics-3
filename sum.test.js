882b7ce136400e154b1c0df247c587f2c9482b58
Gerhard <gerhard.putter@yahoo.com>
2021-11-09 10:28:00 +0200

    Test strings and arrays.

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
 const sum = require("./sum")
-
+//Describe blocks
 describe("example tests", () => {
     it("should add 1 + 2 = 3", () => {
         const result = sum(1, 2);
@@ -15,7 +15,6 @@ describe("example tests", () => {
 })
 
 //Truthy or Falsy Values
-
 describe("truthy or falsy", () => {
     it("null", () => {
         const n = null;
@@ -27,7 +26,6 @@ describe("truthy or falsy", () => {
 })
 
 // Numbers
-
 describe("numbers", () => {
     it("two plus two", () => {
         const value = 2 + 2;
@@ -37,4 +35,22 @@ describe("numbers", () => {
         expect(value).toBeGreaterThanOrEqual(4)
         expect(value).toBeLessThanOrEqual(4)
     })
+    // Decimals
+    it("adding floats", () => {
+        const value = 0.1 + 0.2;
+        expect(value).toBeCloseTo(0.3)
+    })
+})
+
+// Strings
+describe("strings", () => {
+    it("There is no I in team", () => {
+        expect("team").not.toMatch(/I/)
+    })
+})
+
+//Arrays
+describe("arrays", () => {
+    const shoppingLists = ['apples', 'bananas', 'orange', 'strawberry', 'milk']
+    expect(shoppingLists).toContain("milk")
 })
